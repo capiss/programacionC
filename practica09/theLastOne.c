ff37a1ed0be290439950527463e624fcf1c10a01
capiss <armando2hdz@gmail.com>
2017-01-24 17:21:34 -0600

    practica 09

---
+++ practica09/theLastOne.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int atoi(char*);
+
+int main(int argc, char *argv[])
+{
+	if(argc<2)
+        {
+                fprintf(stderr,"Se necesita un argumento!");
+                exit(1349);
+        }
+        printf("%s\n",argv[argc-1]);
+}

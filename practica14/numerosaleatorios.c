8a9e89599b5bb50fad2b126f711894107e254c66
capiss <armando2hdz@gmail.com>
2017-01-26 15:49:02 -0600

    practica 14

--- practica14/numerosaleatorios.c
+++ practica14/numerosaleatorios.c
@@ -5,16 +5,16 @@
 #include<unistd.h>
 int main(int argc, char *argv[])
 {
-        int fd; //Declaracion de un descriptor de archivos.
-        char *archivo="/dev/random";
+	int fd; //Declaracion de un descriptor de archivos.
+	char *archivo="/dev/random";
 	int buffer;
-        fd=open(archivo,O_RDONLY);
+	fd=open(archivo,O_RDONLY);
 
-        if(fd==-1)
-                fprintf(stderr,"No se pudo abrir el archivo");
-        printf("Descriptor de archivo: %d\n",fd);
+	if(fd==-1)
+		fprintf(stderr,"No se pudo abrir el archivo");
+	printf("Descriptor de archivo: %d\n",fd);
 	if(read(fd,&buffer,sizeof(int))!=0)
-                printf("Numero: %i\n",buffer);
+  	printf("Numero: %i\n",buffer);
 	if(close(fd)==-1)
-                fprintf(stderr,"No se pudo cerrar el descriptor de archivo");
+		fprintf(stderr,"No se pudo cerrar el descriptor de archivo");
 }

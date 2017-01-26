b701afb7233b04705e33a644440539d0956d163f
capiss <armando2hdz@gmail.com>
2017-01-26 15:59:19 -0600

    Practica 14

--- practica14/numerosaleatorios.c
+++ practica14/numerosaleatorios.c
@@ -6,15 +6,17 @@
 int main(int argc, char *argv[])
 {
 	int fd; //Declaracion de un descriptor de archivos.
-	char *archivo="/dev/random";
-	int buffer;
-	fd=open(archivo,O_RDONLY);
+	char *archivo="/dev/random"; // archivo que genera aleaotreamente
+	int buffer; //buffer donde se almacena el numero
+	fd=open(archivo,O_RDONLY);// Se abre como solo lectura
 
-	if(fd==-1)
+	if(fd==-1) //valida que abra correctamente
 		fprintf(stderr,"No se pudo abrir el archivo");
+
 	printf("Descriptor de archivo: %d\n",fd);
-	if(read(fd,&buffer,sizeof(int))!=0)
-  	printf("Numero: %i\n",buffer);
-	if(close(fd)==-1)
+	// read regresa el numero de bits que leyo
+	if(read(fd,&buffer,sizeof(int))!=0) //lee desde el archivo 
+	  	printf("Numero: %i\n",buffer); // imprime lo que tiene el archivo
+	if(close(fd)==-1) //debe cerrar.
 		fprintf(stderr,"No se pudo cerrar el descriptor de archivo");
 }

edc47d0285d2e1d258c64c9446148705439df45c
Armando <armando2hdz@gmail.com>
2017-01-30 16:28:40 -0600

    hacker

--- practica06/shrink_me.c
+++ practica06/shrink_me.c
@@ -4,6 +4,6 @@ int main()
 {
 	unsigned int var1,var2;
 	printf("Ingresa dos enteros sin signo(maximo %u): ",UINT_MAX);
-	scanf("%d",&var1,&var2);
+	scanf("%d %d",&var1,&var2);
 	printf("%d",(var1>var2)? var1:var2);
 }

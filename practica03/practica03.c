bd895c66207a05ecf61abc4d27f108c0f6054233
Armando <armando2hdz@gmail.com>
2017-01-30 14:39:00 -0600

    Fin del mundo

---
+++ practica03/practica03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<time.h>
+#include<limits.h>
+
+int main()
+{
+        time_t longt = INT_MAX; //Basta con esta linea  
+        printf("\nEl fin del mundo es: %s\n",ctime(&longt)); //Y esta 
+}
